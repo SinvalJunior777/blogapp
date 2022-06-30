0c6b55c148acc4873dc18872b6094d9e6779cf69
SinvalJunior777 <sinvaljunio123@gmail.com>
2022-06-30 18:50:03 -0300

    teste

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
-if(process.env.NODE_ENV =='production'){
+/*if(process.env.NODE_ENV =='production'){
   module.exports = {mongURI : 'mongodb+srv://junior:C878685c@cluster0.7qz2lxm.mongodb.net/?retryWrites=true&w=majority'}
 }else{
  module.exports = {mongoURI: "mongodb://localhost/blogapp"}
 }
+*/
+module.exports = {mongURI : 'mongodb+srv://junior:C878685c@cluster0.7qz2lxm.mongodb.net/?retryWrites=true&w=majority'}
